a1e7085d251580a9132930a52d4241779a017a56
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-08-21 03:32:43 +0300

    Create Note Model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String description;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.color,
+  });
+}
